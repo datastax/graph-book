0bff3e8fb2028902967192a3a1415a44f3e987db
denisekgosnell@gmail.com <denisegosnell@gmail.com>
2019-12-23 16:19:56 -0500

    dkg: ch 8 notebook + data + loading

---
+++ data/ch8/ch8_load.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# customizable variables
+DSBULK_PATH=/Users/denisegosnell/Desktop/Book/dse-6.8.0_20190911-LABS/dsbulk-1.3.4/bin
+DATADIR=/Users/denisegosnell/Desktop/Book/graph-book/data/ch8
+
+DEST_KS=paths_dev
+
+echo "Loading vertices into the graph $DEST_KS"
+cd $DSBULK_PATH
+./dsbulk load -k $DEST_KS -t Address -url "$DATADIR/Address.csv" -header true
+
+echo "Completed loading vertices into the graph $DEST_KS."
+
+echo "Loading edges into the graph $DEST_KS"
+
+./dsbulk load -k $DEST_KS -t Address__rated__Address -url "$DATADIR/rated.csv" -header true
+
+echo "Completed loading edges into the graph $DEST_KS."
+
