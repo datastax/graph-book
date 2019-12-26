7cc9cfdb29d284f85278ab82b39888059740c6cc
denisekgosnell@gmail.com <denisegosnell@gmail.com>
2019-12-26 13:31:15 -0500

    DKG: fixing edge labels in schema per reviewers comments

--- data/ch10/ch10_load.sh
+++ data/ch10/ch10_load.sh
@@ -49,11 +49,11 @@ echo "Completed loading vertices into the graph $DEST_KS."
 echo "Loading edges into the graph $DEST_KS"
 
 $DSBULK load -k $DEST_KS -t Movie__belongs_to__Genre -url "$DATADIR/belongs_to.csv" -header true
-$DSBULK load -k $DEST_KS -t Movie__topic_tag__Tag -url "$DATADIR/topic_tag_100k_sample.csv" -header true
+$DSBULK load -k $DEST_KS -t Movie__topic_tagged__Tag -url "$DATADIR/topic_tag_100k_sample.csv" -header true
 $DSBULK load -k $DEST_KS -t User__rated__Movie -url "$DATADIR/rated_100k_sample.csv" -header true
 $DSBULK load -k $DEST_KS -t User__tagged__Movie -url "$DATADIR/tagged.csv" -header true
 $DSBULK load -k $DEST_KS -t Actor__acted_in__Movie -url "$DATADIR/acted_in.csv" -header true
-$DSBULK load -k $DEST_KS -t Actor__collaborator__Actor -url "$DATADIR/collaborator.csv" -header true
+$DSBULK load -k $DEST_KS -t Actor__collaborated_with__Actor -url "$DATADIR/collaborator.csv" -header true
 
 echo "Completed loading edges into the graph $DEST_KS."
 
