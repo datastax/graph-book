5c1a31990d8eef8099471caebfa079eb7fee5bf8
Alex Ott <alexott@gmail.com>
2019-12-27 12:52:29 +0100

    Improve specification of variables in data loading scripts
    * DSBulk from the `DSBULK_PATH` takes over the DSBulk from `PATH`
    * Allow to override variables for destination keyspace and DSBulk path via environment,
      without modification of the script itself, like this:
    
    ```
    DSBULK_PATH=/path/to/dsbulk DEST_KS=new_ks data/ch5/ch5_load.sh
    ```

--- data/ch10/ch10_load.sh
+++ data/ch10/ch10_load.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Please set the following variables
-DSBULK_PATH=/PATH/TO/dsbulk-1.3.4
-DEST_KS=movies_dev
+# You may set the following variables inside script, or override corresponding
+# variables when running the script
+DEFAULT_DSBULK_PATH=/PATH/TO/dsbulk-1.3.4
+DEFAULT_DESK_KS=movies_dev
+
+# Allow to override these variables via environment variables set before executing script
+DSBULK_PATH=${DSBULK_PATH:-$DEFAULT_DSBULK_PATH}
+DEST_KS=${DEST_KS:-$DEFAULT_DESK_KS}
 
 ############################################
 # Automated data loading
@@ -13,18 +18,18 @@ DEST_KS=movies_dev
 OLDWD="`pwd`"
 SCRIPTIR="`dirname $0`"
 cd "$SCRIPTDIR"
-DATADIR="`pwd`"
 
 # determine which dsbulk to use, in the case you already have it installed
-DSBULK="`which dsbulk`"
-if [ -z "$DSBULK" ]; then
-    if [ ! -f "$DSBULK_PATH/bin/dsbulk" ]; then
+# the path that is set in the script, takes over the value from PATH
+DSBULK=$DSBULK_PATH/bin/dsbulk
+if [ ! -f "$DSBULK" ]; then
+    DSBULK="`which dsbulk`"
+    if [ -z "$DSBULK" ]; then
         echo "Please set DSBULK_PATH variable to top-level path of DSBulk distribution"
         echo "  or add directory with dsbulk to PATH, like, 'PATH=...dsbulk-1.3.4/bin:\$PATH'"
         exit 1
     fi
 fi
-DSBULK=$DSBULK_PATH/bin/dsbulk
 
 ############################################
 # load the vertices
@@ -35,11 +40,11 @@ echo "Extracting data files for loading..."
 tar xvzf movies_dev_data.tar.gz
 
 echo "Loading vertices into the graph $DEST_KS"
-$DSBULK load -k $DEST_KS -t Movie -url "$DATADIR/Movie.csv" -header true
-$DSBULK load -k $DEST_KS -t User -url "$DATADIR/User.csv" -header true
-$DSBULK load -k $DEST_KS -t Tag -url "$DATADIR/Tag.csv" -header true
-$DSBULK load -k $DEST_KS -t Genre -url "$DATADIR/Genre.csv" -header true
-$DSBULK load -k $DEST_KS -t Actor -url "$DATADIR/Actor.csv" -header true
+$DSBULK load -k $DEST_KS -t Movie -url Movie.csv -header true
+$DSBULK load -k $DEST_KS -t User -url User.csv -header true
+$DSBULK load -k $DEST_KS -t Tag -url Tag.csv -header true
+$DSBULK load -k $DEST_KS -t Genre -url Genre.csv -header true
+$DSBULK load -k $DEST_KS -t Actor -url Actor.csv -header true
 
 echo "Completed loading vertices into the graph $DEST_KS."
 
@@ -48,12 +53,12 @@ echo "Completed loading vertices into the graph $DEST_KS."
 ############################################
 echo "Loading edges into the graph $DEST_KS"
 
-$DSBULK load -k $DEST_KS -t Movie__belongs_to__Genre -url "$DATADIR/belongs_to.csv" -header true
-$DSBULK load -k $DEST_KS -t Movie__topic_tagged__Tag -url "$DATADIR/topic_tag_100k_sample.csv" -header true
-$DSBULK load -k $DEST_KS -t User__rated__Movie -url "$DATADIR/rated_100k_sample.csv" -header true
-$DSBULK load -k $DEST_KS -t User__tagged__Movie -url "$DATADIR/tagged.csv" -header true
-$DSBULK load -k $DEST_KS -t Actor__acted_in__Movie -url "$DATADIR/acted_in.csv" -header true
-$DSBULK load -k $DEST_KS -t Actor__collaborated_with__Actor -url "$DATADIR/collaborator.csv" -header true
+$DSBULK load -k $DEST_KS -t Movie__belongs_to__Genre -url belongs_to.csv -header true
+$DSBULK load -k $DEST_KS -t Movie__topic_tagged__Tag -url topic_tag_100k_sample.csv -header true
+$DSBULK load -k $DEST_KS -t User__rated__Movie -url rated_100k_sample.csv -header true
+$DSBULK load -k $DEST_KS -t User__tagged__Movie -url tagged.csv -header true
+$DSBULK load -k $DEST_KS -t Actor__acted_in__Movie -url acted_in.csv -header true
+$DSBULK load -k $DEST_KS -t Actor__collaborated_with__Actor -url collaborator.csv -header true
 
 echo "Completed loading edges into the graph $DEST_KS."
 
